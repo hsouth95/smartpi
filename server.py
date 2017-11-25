91cc4117dd4d8982418322eefdc2dd37c1aac1d4
Harrison South <hsouth95@hotmail.co.uk>
2017-11-25 17:22:29 +0000

    Added jenkins file

--- server.py
+++ server.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    description = 'No weather data'
+    with open('/tmp/weather.json', 'r') as weather_file:
+        data = json.load(weather_file)
+        description = data['weather'][0]['description']
+
+    return description
